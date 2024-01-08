6ff630c26da4357f832fc7676f131bfa0657b266
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-01-08 18:26:36 +0100

    added delete EditBook and DeleteBook paage

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from 'react-router-dom';
 
-function DeleteBook() {
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+  const handleDeleteBook = () => {
+    setLoading(true)
+    axios
+      .put(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setLoading(false);
+        navigate('/')
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('an error happened. please check console')
+        console.log(error);
+      });
+  }
+
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className="flex flex-col border-2 items-center border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <h3 className="text-2xl">Are u sure u wanna delete this book?</h3>
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onCanPlay={handleDeleteBook}>Yes, Delete it</button>
+      </div>
+    </div>
   )
 }
 
